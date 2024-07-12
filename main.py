b0d7a26821962bb43611eacf24c8b22dac4aec83
PC <polyak.yakov2@gmail.com>
2024-07-12 20:24:01 +0300

    firs capcha

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 import re
+
+from anticaptchaofficial.imagecaptcha import imagecaptcha
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -16,9 +18,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 API_KEY = '217950d4b04f418855a5aa146d3d4a07'
 CSS_SELECTOR_BUTTON = "#mailbox > div.logged-out-one-click.gak__hegm-gzmdja.logged-out-one-click_crossnav > button"
-PATH_MAIL = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/div[2]/div[2]/div[1]/div/div/div/div/div/div[1]/div/input"
-CSS_SELECTOR_PASSWORD = ("#root > div > div > div > div.wrapper-0-2-5 > div > form > div:nth-child(2) > div > "
-                         "div.login-row.password.fill-icon > div > div > div > div > div > input")
+CSS_SELECTOR_TEXT_CAPTCHA = "#app > div > div > form > div.b-panel__content > div > img"
+CSS_SELECTOR_TEXT_CAPTCHA_INPUT = "#app > div > div > form > div.b-panel__content > div > div > input"
+CSS_SELECTOR_CONFIRM_PASSWORD = "#root > div:nth-child(2) > div > div > div > div > form > div:nth-child(2) > div > div:nth-child(3) > div > div > div.submit-button-wrap > div > button"
 
 
 def ExcelWriter(ar):
@@ -35,28 +37,36 @@ def FillArray():
     return mail_phone
 
 
-def SolveCaptcha(h_id):
-    solver = hCaptchaProxyless()
+def SolveCaptcha(driver):
+    solver = imagecaptcha()
     solver.set_verbose(1)
     solver.set_key(API_KEY)
-    solver.set_website_url(
-        "https://id.rambler.ru/login-20/login?rname=head&session=false&back=https%3A%2F%2Fwww.rambler.ru%2F&param=popup&iframeOrigin=https%3A%2F%2Fwww.rambler.ru#startTime=1720530029957")
-    solver.set_website_key(h_id)
     solver.set_soft_id(0)
 
-    g_response = solver.solve_and_return_solution()
-    return g_response
+    captcha_text = solver.solve_and_return_solution("images/captcha.jpeg")
+    driver.find_element(By.CSS_SELECTOR, CSS_SELECTOR_TEXT_CAPTCHA_INPUT).send_keys(captcha_text)
+    driver.find_element(By.XPATH, "//button[span='Продолжить']").click()
 
 
-def LogInMail():
+def CheckSolution(driver):
+    try:
+        error_element = driver.find_element(By.XPATH, "//*[contains(text(), 'Вы указали неправильный код с картинки')]")
+        return False
+    except:
+        return True
+
+
+def LogInMail(mails_count, driver):
     mails = open("mail.txt")
-    for i in mails:
+    for i in range(mails_count):
+        driver.execute_script("window.open('');")
+    windows = driver.window_handles
+    for i, window in zip(mails, windows):
         site_url = "https://account.mail.ru/login"
         mail = i.split(":")[0]
         print(mail)
         password = i.split(":")[1]
-        options = webdriver.ChromeOptions()
-        driver = webdriver.Chrome(options=options)
+        driver.switch_to.window(window)
         driver.get(site_url)
         #WebDriverWait(driver, 90).until((ec.element_to_be_clickable((By.CSS_SELECTOR, CSS_SELECTOR_BUTTON)))).click()
         driver.implicitly_wait(10)
@@ -65,34 +75,24 @@ def LogInMail():
         input_mail.send_keys(mail)
         input_mail.send_keys(Keys.ENTER)
         driver.implicitly_wait(10)
+        time.sleep(2)
         driver.find_element(By.XPATH, "//input[@name='password']").send_keys(password)
-        driver.find_element(By.XPATH, "//input[@name='password']").send_keys(Keys.ENTER)
-        time.sleep(100)
-        try:
-            capt = driver.find_element(By.XPATH,
-                                       "/html/body/div[1]/div/div/div/div/div/div/div[1]/form/section[3]/div/div/div[1]/div/div/iframe")
-            h_id = capt.get_attribute("src")
-            print(h_id)
-            h_id = re.search(r"sitekey\s*=\s*([^\s]*)", h_id).group(1).split("&")[0]
+        driver.find_element(By.CSS_SELECTOR, CSS_SELECTOR_CONFIRM_PASSWORD).click()
+        driver.implicitly_wait(10)
+        text_captcha = driver.find_element(By.CSS_SELECTOR, CSS_SELECTOR_TEXT_CAPTCHA)
+        captcha_url = text_captcha.get_attribute("src")
+        driver.implicitly_wait(10)
+        driver.get(captcha_url)
+        with open("images/captcha.jpeg", 'wb') as file:
+            file.write(driver.find_element(By.TAG_NAME, "img").screenshot_as_png)
+        driver.back()
 
-            print(h_id)
-            result = SolveCaptcha(h_id)
-        except:
-            print("Капчу не попросили")
-
-        driver.execute_script(
-            f'document.querySelector("[data-hcaptcha-response]").setAttribute("data-hcaptcha-response", "{result}");')
-        salt = driver.execute_script(
-            "return document.querySelector('[data-hcaptcha-response]').getAttribute('data-hcaptcha-widget-id')")
-        driver.execute_script(
-            "document.querySelector(" + "'" + '[name="h-captcha-response"]' + "'" + ").innerHTML = " + "'" + result + "'")
-        time.sleep(10)
-        driver.find_element(By.CSS_SELECTOR,
-                            "#__next > div > div > div > div > div > div > div.styles_leftColumn__k_O3r.styles_narrow__6JuTY > form > button").click()
-        time.sleep(10000)
+        while not CheckSolution(driver):
+            SolveCaptcha(driver)
+            time.sleep(2)
 
 
-arr = np.array([[1, 2], [3, 4]])
-ExcelWriter(arr)
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
 mail_phone = FillArray()
-LogInMail()
+LogInMail(1, driver)
--- test.py
+++ test.py
@@ -8,29 +8,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-site_url = "https://2captcha.com/demo/hcaptcha"
+site_url = "https://account.mail.ru/login"
 API_KEY = os.getenv('APIKEY_2CAPTCHA', '297c0a0e1d04d927e810ea6887a3d65f')
 h_id = "a5f74b19-9e45-40e0-b45d-47ff91b7a6c2"
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
 driver.get(site_url)
-
-solver = TwoCaptcha(API_KEY)
-result = solver.hcaptcha(
-            sitekey='3ceb8624-1970-4e6b-91d5-70317b70b651',
-            url=site_url,
-        )
-code = result['code']
-
-
-iframe = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.CSS_SELECTOR,
-                                                                        "#root > div._layout_biy5f_1._basicLayout_18ddp_2 > main > div > div > div._content_18ddp_17 > section > div > form > div._captchaWidgetContainer_1f3oo_22 > div > div > iframe")))
-
-driver.execute_script(
-        "document.querySelector(" + "'" + '[name="h-captcha-response"]' + "'" + ").innerHTML = " + "'" + code + "'")
-driver.execute_script(f'document.querySelector("[data-hcaptcha-response]").setAttribute("data-hcaptcha-response", "{code}");')
-
-driver.find_element(By.CSS_SELECTOR,
-                    "#root > div._layout_biy5f_1._basicLayout_18ddp_2 > main > div > div > div._content_18ddp_17 > section > div > form > div._actions_1f3oo_36 > button._actionsItem_1f3oo_41._buttonPrimary_d46vc_44._button_d46vc_1._buttonMd_d46vc_34").click()
-time.sleep(10000)
+driver.switch_to.window(site_url)
+driver.switch_to.window(site_url)
