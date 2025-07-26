e9e6b6c1c57fd6ea370199267400e096c56c7a5e
Yashik <polyak.yakov22@gmail.com>
2025-07-26 19:38:30 +0300

    bag fix

---
+++ main.py
@@ -0,0 +1,185 @@
+import os.path
+import time
+import re
+
+from anticaptchaofficial.imagecaptcha import imagecaptcha
+from anticaptchaofficial.recaptchav2proxyless import recaptchaV2Proxyless
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+from anticaptchaofficial.hcaptchaproxyless import *
+from twocaptcha import TwoCaptcha
+import pandas as pd
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+
+API_KEY = 'db89b5c66fabea466443ab0e9d81dfdb'
+CSS_SELECTOR_BUTTON = "#mailbox > div.logged-out-one-click.gak__hegm-gzmdja.logged-out-one-click_crossnav > button"
+CSS_SELECTOR_TEXT_CAPTCHA = "#app > div > div > form > div.b-panel__content > div > img"
+CSS_SELECTOR_TEXT_CAPTCHA_INPUT = "#app > div > div > form > div.b-panel__content > div > div > input"
+CSS_SELECTOR_CONFIRM_PASSWORD = "#root > div:nth-child(2) > div > div > div > div > form > div:nth-child(2) > div > div:nth-child(3) > div > div > div.submit-button-wrap > div > button"
+site_url = "https://account.mail.ru/login"
+
+
+def ExcelWriter(ar):
+    df = pd.DataFrame(ar)
+    if os.path.isfile('result.xlsx'):
+        df = pd.concat([df, pd.read_excel('result.xlsx')], ignore_index=True)
+    if 'Unnamed: 0' in df.columns:
+        df.drop('Unnamed: 0', axis=1, inplace=True)
+    df.to_excel('result.xlsx')
+
+
+def FillArray():
+    mails = open("mail.txt")
+    mail_table = []
+    for i in mails:
+        mail_table.append([i.strip().split(":")[0], i.strip().split(":")[1]])
+    return mail_table
+
+
+def SolveTextCaptcha(driver):
+    solver = imagecaptcha()
+    solver.set_verbose(1)
+    solver.set_key(API_KEY)
+    solver.set_soft_id(0)
+
+    captcha_text = solver.solve_and_return_solution("images/captcha.jpeg")
+    driver.find_element(By.CSS_SELECTOR, CSS_SELECTOR_TEXT_CAPTCHA_INPUT).send_keys(captcha_text)
+    driver.find_element(By.XPATH, "//button[span='Продолжить']").click()
+
+
+def SolveReCaptcha(driver):
+    script_element = driver.find_element(By.ID, 'state')
+    script_text = script_element.get_attribute('textContent')
+    data = json.loads(script_text)
+    site_key = data.get('config', {}).get('recaptchaSitekey', '')
+
+    solver = recaptchaV2Proxyless()
+    solver.set_verbose(1)
+    solver.set_key(API_KEY)
+    solver.set_website_url(site_url)
+    solver.set_website_key(site_key)
+
+    g_response = solver.solve_and_return_solution()
+    driver.execute_script(f'document.getElementById("g-recaptcha-response").innerHTML = "{g_response}";')
+
+    symbols = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    for symbol in symbols:
+        try:
+            resp = driver.execute_script(f"return ___grecaptcha_cfg.clients['0']['{symbol}']['{symbol}']")
+            if 'callback' in resp:
+                driver.execute_script(
+                    f"___grecaptcha_cfg.clients['0']['{symbol}']['{symbol}']['callback']('{g_response}')")
+                break
+        except Exception:
+            pass
+
+
+def CheckSolution(driver):
+    try:
+        error_element = driver.find_element(By.XPATH, "//*[contains(text(), 'Вы указали неправильный код с картинки')]")
+        return False
+    except:
+        return True
+
+
+def logIn(driver, mail, password, window):
+    try:
+        input_mail = driver.find_element(By.XPATH, "//input[@name='username']")
+        input_mail.click()
+        input_mail.send_keys(mail)
+        input_mail.send_keys(Keys.ENTER)
+
+        driver.implicitly_wait(5)
+
+
+        try:
+            another_way_vk = driver.find_element(By.CSS_SELECTOR,
+                                                 "#root > div > div > div > div > div > form > div:nth-child(2) > div > div.d1EwXTreBolMwvrmbrWB > button:nth-child(2)")
+            another_way_vk.click()
+        except:
+            pass
+
+        try:
+            skip_button_vk = driver.find_element(By.CSS_SELECTOR, '#root > div > div > div > div > div > form > div:nth-child(2) > div > div.iGVaLv1nniQGGeWuC1H_ > button:nth-child(2)')
+            skip_button_vk.click()
+        except:
+            pass
+
+        driver.implicitly_wait(5)
+        try:
+            iframe = driver.find_element(By.ID, "pass")
+            driver.switch_to.frame(iframe)
+            password_input = driver.find_element(By.CSS_SELECTOR, '#password').send_keys(password)
+            driver.switch_to.default_content()
+            time.sleep(2)
+            button = (driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div/section/form/div[2]/button'))
+            button.click()
+        except Exception as e:
+            print(e)
+            password_input = driver.find_element(By.XPATH, '//input[@name="password"]').send_keys(password)
+            driver.find_element(By.XPATH, '//button[span[text()="Войти"]]').click()
+        return False
+    except Exception as e:
+        print(e)
+        return False
+
+
+def LogInMail(driver):
+    mails = open("mail.txt").readlines()
+    mails_count = len(mails) - 1
+    for i in range(mails_count):
+        driver.execute_script("window.open('');")
+    windows = driver.window_handles
+    for i, window in zip(mails, windows):
+        mail = i.split(":")[0]
+        print(mail)
+        password = i.split(":")[1]
+        try:
+            driver.switch_to.window(window)
+        except:
+            driver.quit()
+        driver.get(site_url)
+        driver.implicitly_wait(5)
+        overlay = driver.find_elements(By.CLASS_NAME, 'vkuiTooltip__overlay')
+        if overlay:
+            driver.execute_script("arguments[0].style.display = 'none';", overlay)
+        while logIn(driver, mail, password, window):
+            driver.implicitly_wait(5)
+
+        try:
+            driver.implicitly_wait(5)
+            driver.find_element(By.XPATH, "//button[span='Это я']").click()
+
+            SolveReCaptcha(driver)
+            driver.find_element(By.XPATH, "//input[@name='password']").send_keys(password)
+        except:
+            try:
+                text_captcha = driver.find_element(By.CSS_SELECTOR, CSS_SELECTOR_TEXT_CAPTCHA)
+                captcha_url = text_captcha.get_attribute("src")
+                driver.implicitly_wait(5)
+                driver.get(captcha_url)
+                with open("images/captcha.jpeg", 'wb') as file:
+                    file.write(driver.find_element(By.TAG_NAME, "img").screenshot_as_png)
+                driver.back()
+
+                while not CheckSolution(driver):
+                    SolveTextCaptcha(driver)
+                    time.sleep(2)
+            except:
+                print("капчу не попросили")
+
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
+mail_phone = FillArray()
+ExcelWriter(mail_phone)
+LogInMail(driver)
+input("Нажмите Enter, чтобы закрыть браузер и завершить работу...")
+driver.quit()
